c3d587e972793b9df752042ecc59d4ced46d7515
Agustín Aliaga Casaletti <agustinaliagac@gmail.com>
2020-06-30 20:18:36 -0300

    Parallelize goroutines for HTTP and gRPC (HTTP2) servers

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -20,38 +20,25 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net"
+	"sync"
 
-	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"github.com/agustinaliagac/firewatch/internal/grpc"
+	"github.com/agustinaliagac/firewatch/internal/rest"
 )
-
-const (
-	port = ":50051"
-)
-
-// server is used to implement helloworld.GreeterServer.
-type server struct {
-	pb.UnimplementedGreeterServer
-}
-
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	log.Printf("Size %v", in.XXX_Size())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
 
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	go func() {
+		rest.StartServer()
+		wg.Done()
+	}()
+
+	go func() {
+		grpc.StartServer()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
